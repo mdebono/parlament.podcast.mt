6c8ac8fdc6341ee37591d7bd82018f85adb84809
Michael Debono Mrđen <michael@xn--debonomren-2kb.eu>
2026-03-18 23:25:19 +0100

    Update reverse episodes

--- parlament/app.py
+++ parlament/app.py
@@ -7,7 +7,7 @@ def run():
 
     feed = pfeed.init_feed()
     seen_guids = set()
-    for sitting in sittings:
+    for sitting in reversed(sittings):
         audio_url = papi.get_sitting_audio_url(sitting)
         audio_url = papi.correct_audio_url(sitting, audio_url)
         if audio_url in seen_guids:
