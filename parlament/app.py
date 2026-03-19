98517461c6b4266f5cb73123bba5c647bb1b31ed
Michael Debono Mrđen <michael@xn--debonomren-2kb.eu>
2026-03-19 01:01:57 +0100

    Update app.py

--- parlament/app.py
+++ parlament/app.py
@@ -7,14 +7,17 @@ def run():
 
     feed = pfeed.init_feed()
     seen_guids = set()
-    for sitting in list(reversed(sittings))[:100]:
+    for i,sitting in enumerate(list(reversed(sittings))[:100]):
         audio_url = papi.get_sitting_audio_url(sitting)
         audio_url = papi.correct_audio_url(sitting, audio_url)
         if audio_url in seen_guids:
             print(f"WARNING: Duplicate GUID for sitting {papi.get_sitting_number(sitting)}: {audio_url}", file=sys.stderr)
         seen_guids.add(audio_url)
+        title = papi.get_episode_title(leg, sitting)
+        if i == 1:
+          title = title + '-'
         pfeed.add_item(feed,
-            title = papi.get_episode_title(leg, sitting),
+            title = title,
             description = papi.get_episode_description(leg, sitting),
             link = papi.get_sitting_url(sitting),
             audio_url = audio_url,
